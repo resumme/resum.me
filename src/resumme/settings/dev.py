709e3af04e3141aa6d6f994008d22b1936cb1637
adriancast <adrian.castellanos@roomdi.com>
2020-03-17 13:19:51 +0100

    Added sql lite to the dev settings & requirements

--- src/resumme/settings/dev.py
+++ src/resumme/settings/dev.py
@@ -14,11 +14,8 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.db',
     }
 }
 
